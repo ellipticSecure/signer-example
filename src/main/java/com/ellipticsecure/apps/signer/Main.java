72abb3063cf21bc00c9252e5ac698bf41b7b5006
Kobus Grobler <kobus.grobler@gmail.com>
2020-07-23 12:44:15 +0200

    V0.0.4 Use pom version in GitHub action for releases

--- src/main/java/com/ellipticsecure/apps/signer/Main.java
+++ src/main/java/com/ellipticsecure/apps/signer/Main.java
@@ -15,11 +15,19 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 /**
  * Signer example app Main class
  * @author Kobus Grobler
  */
 public class Main extends Application {
+
+    private static String version ="DEV";
 
     private static Main instance;
 
@@ -30,13 +38,29 @@ public class Main extends Application {
         instance = this;
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon_512x512.png")));
-        primaryStage.setTitle("ellipticSecure PDF Signer v0.2");
+        primaryStage.setTitle("ellipticSecure PDF Signer "+version);
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        setVersion();
         launch(args);
+    }
+
+    private static void setVersion() throws IOException {
+        Class<Main> clazz = Main.class;
+        String className = clazz.getSimpleName() + ".class";
+        String classPath = clazz.getResource(className).toString();
+        if (classPath.startsWith("jar")) {
+            String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) +
+                    "/META-INF/MANIFEST.MF";
+            try (InputStream in = new URL(manifestPath).openStream()) {
+                Manifest manifest = new Manifest(in);
+                Attributes attr = manifest.getMainAttributes();
+                version = attr.getValue("Implementation-Version");
+            }
+        }
     }
 }
